829250fb3d3ad31002b22ba7b08825fddd26d27c
Chiara JF <51326610+chiarajf@users.noreply.github.com>
2023-11-01 11:20:05 +0100

    Add files via upload

---
+++ 01-11-23/script.js
@@ -0,0 +1,14 @@
+const animalindex = [
+  "cat",
+  "dog",
+  "mouse",
+  "bird",
+  "turtle",
+  "pig",
+  "caw",
+  "horse",
+];
+
+for (let i = 0; i < animalindex.length; i++) {
+  console.log(i + ":" + animalindex[i]);
+}
