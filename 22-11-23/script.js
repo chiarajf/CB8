066728d1a54a18bdd74270aba9f22430ca7bc245
Chiara JF <51326610+chiarajf@users.noreply.github.com>
2023-11-22 16:20:03 +0100

    Add files via upload

---
+++ 22-11-23/script.js
@@ -0,0 +1,52 @@
+//ESERCIZIO 1
+
+const asyncFetch = async() =>{
+    const res = await fetch("https://picsum.photos/v2/list") ;
+    const data = await res.json();
+    const filterImg = data.map((image) => console.log(image.download_url));
+    return filterImg
+}
+
+asyncFetch();
+
+
+// ESERCIZIO 2
+
+const object = {
+  id: "0",
+  author: "Alejandro Escamilla",
+  download_url: "https://picsum.photos/id/0/5000/3333",
+};
+
+const createImgTxt = (obj) =>{
+    const container = document.createElement ("div");
+    const img = document.createElement("img");
+    const txt = document.createElement("h1");
+
+    img.src = obj.download_url;
+    txt.textContent = obj.author;
+
+    container.append(img, txt)
+
+    return container;
+}
+
+document.body.appendChild(createImgTxt(object))
+
+//AVANZATO
+
+const secondFetch = async () => {
+  const res = await fetch("https://picsum.photos/v2/list");
+  const data = await res.json();
+
+  return data;
+};
+
+const allImage = async()=>{
+    const data = await secondFetch();
+    data.forEach((obj) =>{
+        const card = createImgTxt(obj);
+        document.body.append(card)
+    })
+}
+allImage()
