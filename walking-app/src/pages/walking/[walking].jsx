d15ec3b76774e49c15eb9f93886fc4f3e3d39338
ChiaraJF <chiara.fell87@gmail.com>
2024-02-11 22:28:33 +0100

    modifica

--- walking-app/src/pages/walking/[walking].jsx
+++ walking-app/src/pages/walking/[walking].jsx
@@ -67,17 +67,6 @@ export default function Walking() {
     }
   };
 
-  const saveProgress = () => {
-    const currentDayData = walkData[`day${currentDay}`];
-    if (currentDayData) {
-      const updatedDailyProgress = [
-        ...dailyProgress,
-        { day: currentDay, data: currentDayData },
-      ];
-      setDailyProgress(updatedDailyProgress);
-    }
-  };
-
   const handleSaveProgress = () => {
     const currentDayData = walkData[`day${currentDay}`];
     if (currentDayData) {
@@ -95,6 +84,7 @@ export default function Walking() {
 
   const handleGoToProgress = () => {
     router.push("/progress");
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   if (!filteredData) {
